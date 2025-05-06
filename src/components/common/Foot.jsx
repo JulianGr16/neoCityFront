2bc02179e152a834ed4b741b90d3519e6ac7fa7d
JulianGr16 <juliangaray.2116@gmail.com>
2025-05-06 01:55:42 -0300

    termine el footer y quedo totalmente responsivo

--- src/components/common/Foot.jsx
+++ src/components/common/Foot.jsx
@@ -4,41 +4,55 @@ import '../../App.css'
 
 const Foot = () => {
     return (
-        <footer className='container'>
-            <section className='row py-4'>
-                <article className='col-sm-12 col-md-4 fs-4'>
+        <footer className='container-fluid bg-body-secondary'>
+            <section className='row py-4 text-center'>
+                <article className='col-sm-12 col-md-3 fs-4'>
                     <h4>Redes Social</h4>
-                   <div className='d-flex'>
-                   <div className='ms-2'>
+                   <div className='d-flex justify-content-around'>
+                   <div className='fs-3'>
                    <i class="bi bi-instagram"></i>
                    </div>
-                    <div className='mx-2'>
+                    <div className='fs-3'>
                     <i class="bi bi-linkedin"></i>
                     </div>
-                    <div>
+                    <div className='fs-3'>
                     <i class="bi bi-facebook"></i>
                     </div>
-                    <div className='ms-2'>
+                    <div className='fs-3'>
                     <i class="bi bi-twitter-x"></i>
                     </div>
                    </div>
                 </article>
-                <article className='col-sm-12 col-md-4 text-center'>
-                    <h4>Preguntas Frecuentes</h4>
-                    <ul className=' lista-preguntas'>
-                        <li>¿Cómo puedo hacer una reserva?</li>
-                        <li>¿Puedo cancelar o modificar mi reserva?</li>
-                        <li>¿Cuáles son los horarios de check-in y check-out?</li>
-                        <li>¿Se permite el ingreso de mascotas?</li>
-                        <li>¿El hotel cuenta con estacionamiento?</li>
-                    </ul>
+                <article className='col-sm-12 col-md-3 '>
+                    <h4>Enlaces útiles</h4>
+                    <div className=' lista-preguntas'>
+                        <li>Política de privacidad</li>
+                        <li>Términos y condiciones</li>
+                        <li>Preguntas frecuentes</li>
+                        <li></li>
+                    </div>
                 </article>
-                <article className='col-sm-12 col-md-4'>
-                    <h4>Estamos ubicados <i class="bi bi-geo-alt"></i></h4>
+                <article className='col-sm-12 col-md-3'>
+                    <h4>Servicios y amenities</h4>
+                    <div className=' lista-preguntas'>
+                        <li>Wifi grauito</li>
+                        <li>Gimnasio</li>
+                        <li>Piscina</li>
+                        <li>Restaurante</li>
+                    </div>
+                </article>
+                <article className='col-sm-12 col-md-3'>
+                    <h4>Información del contacto</h4>
+                    <div className=' lista-preguntas'>
+                        <li>Dirección:Salta 78 - San Miguel de Tucuman</li>
+                        <li>Teléfono:+5438155689</li>
+                        <li>Correo electrónico:info@hotel.com</li>
+                        <li>Restaurante</li>
+                    </div>
                 </article>
             </section>
-            <section className='text-center border-top'>
-                <h6 className='mt-2'>TODOS LOS DERECHOS RESERVADOS</h6>
+            <section className='text-center  border-top border-black pb-2'>
+                <h6 className='mt-2'>©TODOS LOS DERECHOS RESERVADOS</h6>
                 
             </section>
         </footer>
