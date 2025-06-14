5f717dbb8d8c6463eab5f9baaea36e1d81699383
JulianGr16 <juliangaray.2116@gmail.com>
2025-06-13 22:20:30 -0300

    realizamos detalles esteticos en la rama registro

--- src/components/pages/Bienvenidos.jsx
+++ src/components/pages/Bienvenidos.jsx
@@ -1,14 +1,16 @@
 import { Container } from "react-bootstrap";
 import "../../App.css"
+import { NavLink } from "react-router-dom";
 
 const Bienvenidos = () => {
     return (
         <Container className="flex-grow-1 mt-5">
-            <div className="row">
+            <article className="row border py-5 px-5 rounded border-black bg-body-secondary mb-4">
                 <div className="col-sm-12 col-md-12">
                     <h2>¡Bienvenido a NeoCity! Ya podes empezar a realizar tus Reservas 😊</h2>
+                    <NavLink to="/login" className="btn btn-primary mt-1 text-center">Iniciar sesion</NavLink>
                 </div>
-            </div>
+            </article>
         </Container>
     );
 };
